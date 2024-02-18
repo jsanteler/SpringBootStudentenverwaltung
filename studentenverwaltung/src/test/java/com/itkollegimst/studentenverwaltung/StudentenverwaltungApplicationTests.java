80040cea6f63e1a7a1583d02d479882da7eb6d19
jsanteler <juliansanteler@tsn.at>
2024-02-18 15:40:48 +0100

    Projectstart

---
+++ studentenverwaltung/src/test/java/com/itkollegimst/studentenverwaltung/StudentenverwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itkollegimst.studentenverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentenverwaltungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
